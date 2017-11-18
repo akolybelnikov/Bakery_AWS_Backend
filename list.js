a6487400cc9a1be492dcdc50e0dd7609fe857685
akolybelnikov <a.kolybelnikov@gmail.com>
2017-11-18 23:35:32 +0100

    querying products by category name

--- list.js
+++ list.js
@@ -4,14 +4,6 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context, callback) {
   const params = {
     TableName: "categories",
-    //ProjectionExpression: "productId, attachment, category, content, price, productName"
-    // FilterExpression: "category",
-    // 'KeyConditionExpression' defines the condition for the query
-    // - 'userId = :userId': only return items with matching 'userId'
-    //   partition key
-    // 'ExpressionAttributeValues' defines the value in the condition
-    // - ':userId': defines 'userId' to be Identity Pool identity id
-    //   of the authenticated user
     KeyConditionExpression: "userId = :userId",
     ExpressionAttributeValues: {
       ":userId": "eu-central-1:e393fcdf-4bba-4a36-b844-c8bfafc20fc7"
@@ -31,17 +23,17 @@ export async function main(event, context, callback) {
 export async function products(event, context, callback) {
   const params = {
     TableName: "products",
-    KeyConditionExpression: "userId = :userId",
+    IndexName: "category-index",
+    KeyConditionExpression: "category = :category",
     ExpressionAttributeValues: {
-      ":userId": "eu-central-1:e393fcdf-4bba-4a36-b844-c8bfafc20fc7"
+      ":category": event.pathParameters.id
     },
-    Key: {
-      category: event.pathParameters.id
-    }
+    "ScanIndexForward": false
   };
 
   try {
     const result = await dynamoDbLib.call("query", params);
+
     // Return the list of products in response body
     callback(null, success(result.Items));
   } catch (e) {
