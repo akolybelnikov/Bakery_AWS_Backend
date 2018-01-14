3a64254e7d246bc42a276ebcddf66e58f211b5c0
Andrey Kolybelnikov <a.kolybelnikov@googlemail.com>
2018-01-14 19:31:15 +0100

    removing cached files

--- libs/dynamodb-lib.js
+++
@@ -1,8 +0,0 @@
-import AWS from "aws-sdk";
-
-AWS.config.update({ region: "eu-central-1" });
-
-export function call(action, params) {
-    const dynamoDb = new AWS.DynamoDB.DocumentClient();
-    return dynamoDb[action](params).promise();
-}
